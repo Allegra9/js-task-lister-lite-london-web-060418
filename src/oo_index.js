4159d34b15ae5460615dbbd609aba7a2570fbf08
Allegra <aukseallegra@gmail.com>
2018-07-23 15:08:01 +0100

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,34 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
+
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  const htmlTaskList = document.querySelector("#list #tasks");
+
+  //event we listen for, the function to handle that event
+  newTaskForm.addEventListener("submit", createTask)
+
+  function createTask(e) {
+    e.preventDefault();
+    let li = document.createElement("li");
+    let btn = document.createElement("button");
+    btn.addEventListener("click", deleteTask)
+
+    btn.innerText = "X"
+    li.innerText = `${newTaskDescription.value} `
+
+    htmlTaskList.append(li);
+    li.append(btn);
+
+    function deleteTask(e){
+      li.remove()
+    }
+    e.target.reset();
+  }
+
+  // function deleteTask(e){
+  //   console.log(e)
+  //   e.path[1].remove()
+  // }
+
 });
--- src/taskList.js
+++ src/taskList.js
@@ -1,2 +1,12 @@
 class TaskList {
 }
+
+
+
+
+//if I wanted to take an input from html form:
+// const input = document.querySelector("input")
+//
+// input.addEventListener("keydown", function(e) {
+//   console.log(e.which)
+// })
